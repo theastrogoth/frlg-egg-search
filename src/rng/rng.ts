ffb377f9769313c2f0ba800c483c6061b0375fa2
theastrogoth <theastrogoth@gmail.com>
2026-06-10 09:28:25 -0500

    fix gender calculation

--- src/rng/rng.ts
+++ src/rng/rng.ts
@@ -60,7 +60,7 @@ function generatePID(state1: bigint, state2: bigint): bigint {
 }
 
 function genderFromPID(pid: bigint): number {
-    return Number(pid && 0xffn);
+    return Number(pid % 0xffn);
 }
 
 export function natureFromPID(pid: bigint): Nature {
